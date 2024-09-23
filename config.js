7c3d2967f9a43e95d3b5bfd4db3cb5e6a43b2026
Xoul ser <131429508+sabariy@users.noreply.github.com>
2024-09-23 07:46:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 const toBool = (x) => x == 'true'
 module.exports = {
-  HANDLERS: (process.env.HANDLERS || '^[.,!]').trim(),
+  HANDLERS: (process.env.HANDLERS || '.').trim(),
   MODE: (process.env.MODE || 'public').toLowerCase(),
   READ_CMD: toBool(process.env.READ_CMD),
   READ_MSG: toBool(process.env.READ_MSG),
-  SUDO: process.env.SUDO || '',
+  SUDO: (process.env.SUDO || '',
   SESSION_ID : process.env.SESSION_ID || "Fenix-Md&bBrftXjL";
 };
